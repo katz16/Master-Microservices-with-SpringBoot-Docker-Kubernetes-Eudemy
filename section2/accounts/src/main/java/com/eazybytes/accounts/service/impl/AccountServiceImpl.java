4e8ada212ba63d0e7a02cb3dadd20577fd955a28
katz16 <karthikkumar654@gmail.com>
2026-02-08 14:53:37 +0530

    Constants, Mapper and Service implemented

---
+++ section2/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountServiceImpl.java
@@ -0,0 +1,26 @@
+package com.eazybytes.accounts.service.impl;
+
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.repository.AccountsRepository;
+import com.eazybytes.accounts.repository.CustomerRepository;
+import com.eazybytes.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AccountServiceImpl implements IAccountsService {
+    @Autowired
+    private AccountsRepository accountsRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+    /**
+     *
+     * @param customerDto - CustomerDto object
+     */
+    @Override
+    public void createAccount(CustomerDto customerDto) {
+
+    }
+}
